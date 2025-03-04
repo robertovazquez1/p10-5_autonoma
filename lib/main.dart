d089e90ff9cf5de64ccbbdd5a9156762c75960a5
VickRobin <a.22308051281326@cbtis128.edu.mx>
2025-03-04 17:15:14 +0000

    auto

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,96 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        centerTitle: true,
+        foregroundColor: Colors.white,
+        title: Text("Container 1"),
+        backgroundColor: Colors.blue,
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Roberto Vazquez Avendaño 22308051281326",
+                style: TextStyle(fontSize: 20)),
+            SizedBox(
+              height: 75,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all()),
+            ),
+            SizedBox(height: 20),
+            // Rectángulo Rojo con Borde Punteado
+            DottedBorder(
+              borderType: BorderType.RRect,
+              radius: Radius.circular(15),
+              dashPattern: [6, 3],
+              color: Colors.black,
+              strokeWidth: 2,
+              child: Container(
+                width: 100,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Círculo Naranja con Borde Punteado
+            DottedBorder(
+              borderType: BorderType.Circle,
+              dashPattern: [6, 3],
+              color: Colors.black,
+              strokeWidth: 2,
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // FlutterLogo con Borde Punteado Circular
+            DottedBorder(
+              borderType: BorderType.Circle,
+              dashPattern: [6, 3],
+              color: Colors.black,
+              strokeWidth: 2,
+              child: Padding(
+                padding: EdgeInsets.all(8), // Espaciado dentro del borde
+                child: FlutterLogo(size: 60),
+              ),
+            ),
+          ],
         ),
       ),
     );
